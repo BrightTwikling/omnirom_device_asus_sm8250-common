efdbf0127c80606e5cdecc35651a6f3aebd03ea5
micky387 <mickaelsaibi@free.fr>
2024-07-17 09:28:44 -0400

    sm8250-common: Drop WFD libwfdnative symlink
    after https://github.com/omnirom/android_device_asus_zenfone7/commit/9a2fb3a8bed22c1058e8d69fc3bb3d381686cc7b
    
    Change-Id: I51f1fa772725e3394fb486d47e8bc0b02da4d89f

--- Android.mk
+++ Android.mk
@@ -53,15 +53,6 @@ $(SYSHELPER_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(SYSHELPER_SYMLINKS)
 
-WFD_LIB := libwfdnative.so
-WFD_SYMLINKS := $(addprefix $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/WfdService/lib/arm64/,$(notdir $(WFD_LIB)))
-$(WFD_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "WFD lib link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /system_ext/lib64/$(notdir $@) $@
-ALL_DEFAULT_INSTALLED_MODULES += $(WFD_SYMLINKS)
-
 FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/firmware_mnt
 $(FIRMWARE_MOUNT_POINT): $(LOCAL_INSTALLED_MODULE)
 	@echo "Creating $(FIRMWARE_MOUNT_POINT)"
