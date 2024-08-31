b3ef3cba0a566eb94679f27a4c313d98e583200d
BrightTwikling <DF.SJbbba5@gmail.com>
2024-08-31 22:00:27 +0900

    Change including method of vendor/omni/qcom/common.mk
    Signed-off-by: BrightTwikling <DF.SJbbba5@gmail.com>

--- omni_common.mk
+++ omni_common.mk
@@ -1,5 +1,10 @@
 # Add common definitions for Qualcomm
-$(call inherit-product, vendor/omni/qcom/common.mk)
+vendor_omni_qcom_common=$(wildcard vendor/omni/qcom/common.mk)
+ifneq ($(vendor_omni_qcom_common),)
+include vendor/omni/qcom/common.mk
+else
+include device/asus/sm8250-common/vendor/omni/qcom/common.mk
+endif
 
 # Platform
 TARGET_BOARD_PLATFORM := kona
